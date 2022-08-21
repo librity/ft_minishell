626a8f0638f85a0b72d98be496bb7a9d3e6b62ed
Luis <luis.geniole@gmail.com>
2022-08-21 11:47:11 -0300

    Fix 42 header.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 10:34:20 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/08/21 01:13:25 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/08/21 11:44:34 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,8 @@ static void	repl(void)
 	lex("infile   < tr a   \'  \"   \' |   tr \' \"     \' x > outfile");
 	lex("infile   < tr a   \'  \"   \' |   tr \' \"      x > outfile");
 	lex("inf'ile");
-	lex("MAKE_PATH=./libs/libft ; cd .. ; ls && make -C $MAKE_PATH ; ./minshell");
+	lex("MAKE_PATH=./libs/libft ; cd .. ; ls && make -C $MAKE_PATH ; \
+./minshell");
 	lex(".gitignore < tr ex ' X' | tr pi 'P ' > outfile");
 }
 
