a035b43bd3958ca51394b7224b00a9b3d8e66e4b
Luis <luis.geniole@gmail.com>
2022-08-28 22:05:48 -0300

    Restore get_user_line.

--- sources/mandatory/teste.c
+++ sources/mandatory/teste.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 16:39:46 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/08/28 20:10:32 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/08/28 22:05:15 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 char	*get_user_line(void)
 {
-	char	*line;
+	// char	*line;
 
-	line = readline(">");
-	return (line);
+	// line = readline(">");
+	// return (line);
+	char	*buffer;
+	int		size;
+
+	buffer = (char *)ft_salloc(FT_ARG_MAX);
+	size = read(STDIN_FILENO, buffer, FT_ARG_MAX);
+	buffer[size - 1] = '\0';
+	return(buffer);
 }
