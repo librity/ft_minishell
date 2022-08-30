e56cbda75a2ddff5c4aa9b28d5562d4ef71fbe5d
Luis <luis.geniole@gmail.com>
2022-08-30 18:31:30 -0300

    Refactor ht_insert.
    Co-authored-by: wagraton <Wagratom@users.noreply.github.com>

--- sources/mandatory/hash_table/insert.c
+++ sources/mandatory/hash_table/insert.c
@@ -6,24 +6,28 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 16:17:14 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/08/30 18:23:48 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/08/30 18:30:51 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-t_ht_item	*find_by_key(t_dlist *lst, char *key)
+static bool	handled_reinsert(t_dlist *node, char *key, char *value)
 {
 	t_ht_item	*item;
 
-	while (lst)
+	while (node != NULL)
 	{
-		item = lst->content;
+		item = node->content;
 		if (ft_streq(item->key, key))
-			return (item);
-		lst = lst->next;
+		{
+			free(item->value);
+			item->value = ft_strdup(value);
+			return (true);
+		}
+		node = node->next;
 	}
-	return (NULL);
+	return (false);
 }
 
 void	ht_insert(t_hash_table *table, char *key, char *value)
@@ -31,17 +35,11 @@ void	ht_insert(t_hash_table *table, char *key, char *value)
 	int			index;
 	t_ht_item	*new_item;
 	t_dlist		**index_list;
-	t_ht_item	*item;
 
 	index = ht_get_index(key);
 	index_list = &table->index_lists[index];
-	item = find_by_key(*index_list, key);
-	if (item)
-	{
-		free(item->value);
-		item->value = ft_strdup(value);
+	if (handled_reinsert(*index_list, key, value))
 		return ;
-	}
 	new_item = ht_new_item(key, value);
 	ft_dlst_addb(index_list, new_item);
 	table->count++;
