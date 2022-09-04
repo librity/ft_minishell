c539c0dde95a7289781b7df45737cd3fa89a5760
Bankai <wagratom111@gmail.com>
2022-09-04 14:10:11 -0300

    edit prm acess  X_OK

--- sources/mandatory/readline/get_cmd_path.c
+++ sources/mandatory/readline/get_cmd_path.c
@@ -6,7 +6,7 @@
 /*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 16:44:43 by wwallas-          #+#    #+#             */
-/*   Updated: 2022/09/04 00:30:54 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/09/04 14:09:10 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*get_cmd_path(char *cmd)
 	{
 		script = ft_strjoin(rtn_path_index(index), "/");
 		script = ft_strjoin_del(&script, cmd);
-		if (!access(script, F_OK))
+		if (!access(script,  X_OK))
 			return (script);
 		free(script);
 		index++;
