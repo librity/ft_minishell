884a916ca96dc75c4e64bdc8bd46faec0b293c26
wallas <wagratom111@gmail.com>
2022-10-07 22:57:07 -0300

    new tests pipeline

--- tests/executor/pipeline.c
+++ tests/executor/pipeline.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipeline.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 14:22:08 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/10/06 17:00:18 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2022/10/07 22:56:51 by wwallas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,15 +63,60 @@ MU_TEST(ep_e_tst)
 	execute_pipeline(_plist);
 }
 
+MU_TEST(etpet_e_tst)
+{
+	_plist = NULL;
+
+	add_truncate(&_plist, "teste");
+	add_exec(&_plist, (char *[]){"ls", "-la", NULL});
+	add_pipe(&_plist);
+	add_truncate(&_plist, "teste");
+	add_exec(&_plist, (char *[]){"ls", NULL});
+	execute_pipeline(_plist);
+}
+
+MU_TEST(etpete_e_tst)
+{
+	_plist = NULL;
+
+	add_truncate(&_plist, "teste");
+	add_exec(&_plist, (char *[]){"ls", "-la", NULL});
+	add_pipe(&_plist);
+
+	add_truncate(&_plist, "teste");
+	add_exec(&_plist, (char *[]){"ls", NULL});
+	add_pipe(&_plist);
+
+	add_truncate(&_plist, "teste");
+	add_exec(&_plist, (char *[]){"echo", "wallas", NULL});
+	execute_pipeline(_plist);
+}
+
+MU_TEST(pera_e_tst)
+{
+	_plist = NULL;
+
+	add_read_file(&_plist, "teste2");
+	add_exec(&_plist, (char *[]){"tr", "w", "a", NULL});
+	add_pipe(&_plist);
+
+	add_truncate(&_plist, "teste2");
+	add_exec(&_plist, (char *[]){"tr", "w", "b", NULL});
+	execute_pipeline(_plist);
+}
+
 MU_TEST_SUITE(pipeline_suite)
 {
 	MU_SUITE_CONFIGURE(&setup, &teardown);
 
-	MU_RUN_TEST(e_tst);
-	MU_RUN_TEST(ls_multiple_arg_tst);
-	MU_RUN_TEST(ls_err_tst);
+	//MU_RUN_TEST(e_tst);
+	//MU_RUN_TEST(ls_multiple_arg_tst);
+	//MU_RUN_TEST(ls_err_tst);
 
-	MU_RUN_TEST(ep_e_tst);
+	//MU_RUN_TEST(ep_e_tst);
+	//MU_RUN_TEST(etpet_e_tst); //acho que a saida esta diferente
+	//MU_RUN_TEST(etpete_e_tst); // printou um esq aqui wtf
+	//MU_RUN_TEST(pera_e_tst); acho que deveria printar no terminal
 }
 
 MU_MAIN
