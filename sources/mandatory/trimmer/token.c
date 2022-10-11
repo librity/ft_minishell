a8356afaa2b1f4cc8bdfb19ee2963f2424eac4f9
Luis <luis.geniole@gmail.com>
2022-10-11 15:36:19 -0300

    Fix empty quotes bug.

--- sources/mandatory/trimmer/token.c
+++ sources/mandatory/trimmer/token.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wwallas- <wwallas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 20:31:19 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2022/10/11 10:51:48 by wwallas-         ###   ########.fr       */
+/*   Updated: 2022/10/11 15:33:06 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ static bool	handled_single_quote(char **token)
 	if (!at_single_quote(*token))
 		return (false);
 	strbuff_cut(*token);
+	if (at_single_quote(*token))
+	{
+		strbuff_cut(*token);
+		return (true);
+	}
 	*token = skip_single_quotes(*token);
 	if (*token != NULL && at_single_quote(*token))
 		strbuff_cut(*token);
@@ -28,6 +33,11 @@ static bool	handled_double_quote(char **token)
 	if (!at_double_quote(*token))
 		return (false);
 	strbuff_cut(*token);
+	if (at_double_quote(*token))
+	{
+		strbuff_cut(*token);
+		return (true);
+	}
 	*token = skip_double_quotes(*token);
 	if (*token != NULL && at_double_quote(*token))
 		strbuff_cut(*token);
